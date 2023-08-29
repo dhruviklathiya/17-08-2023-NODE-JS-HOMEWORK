894b8b0cb672a694ba33a39c4f7740e952c5e194
DHRUVIK LATHIYA <dhruviklathiya9999@gmail.com>
2023-08-29 13:58:51 +0530

    comments in mobile

--- src/services/mobile.service.js
+++ src/services/mobile.service.js
@@ -1,36 +1,37 @@
 const { Mobile } = require("../models");
-
+/**GET MOBILE BY NAME */
 const get_mobile_by_name = async(mobile_name) => {
     return Mobile.findOne({mobile_name});
 }
-
+/**CREATE MOBILE */
 const create_mobile = async(reqbody) => {
     return Mobile.create(reqbody);
 }
-
+/**GET MOBILE LIST */
 const get_mobile_list = async() => {
     return Mobile.find();
 }
-
+/**GET MOBILE BY ID */
 const get_mobile_by_id = async(mobile_id) => {
     return Mobile.findById(mobile_id);
 }
-
+/**PROVIDES STATUS IN CONTROLLER */
 const get_mobile_status = async(mobile_id) => {
-    const mobile__ = Mobile.findById(mobile_id);
-    return mobile__;
+    const mobile_detail = Mobile.findById(mobile_id);
+    return mobile_detail;
 }
-
+/**UPDATE MOBILE STATE */
 const update_mobile_status = async(mobile_id,mobile_status) => {
     if(mobile_status){
         return Mobile.findByIdAndUpdate(mobile_id,{$set: {is_active:false}});
     }
     return Mobile.findByIdAndUpdate(mobile_id,{$set: {is_active:true}});
 }
+/**UPDATE MOBILE DETAILS */
 const update_mobile = async(mobile_id,reqbody) => {
     return Mobile.findByIdAndUpdate(mobile_id,{$set: reqbody});
 }
-
+/**DELETE MOBILE BY ID */
 const delete_mobile = async(mobile_id) => {
     return Mobile.findByIdAndDelete(mobile_id);
 }
